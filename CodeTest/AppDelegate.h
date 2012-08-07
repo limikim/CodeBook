ad4ab340437aa97c36eb6f75f0db7cb243978576
김 정희 <limikim@gmail.com>
2012-08-07 19:22:33 +0900

    Initial Commit

---
+++ CodeTest/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  CodeTest
+//
+//  Created by 정희 김 on 12. 8. 7..
+//  Copyright (c) 2012년 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
